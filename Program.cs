46d8201450c80a6e1c4f4e76e21001cc075f8e94
Женя <esamoilov@yandex.ru>
2023-12-02 00:32:28 +0300

    добавил 4 строчку

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("22 Hello, World!");
+Console.WriteLine("33 Hello, World!");
+Console.WriteLine("33 Hello, World!");
 Console.WriteLine("22 Hello, World!");
 Console.WriteLine("22 Hello, World!");
